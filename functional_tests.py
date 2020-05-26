a9434b62cc179cf9feda04517d66ae16959e2704
xx <726622695@qq.com>
2020-05-26 21:49:08 +0800

    modified:   functional_tests.py 	modified:   geckodriver.log 	modified:   lists/__pycache__/tests.cpython-35.pyc 	modified:   lists/__pycache__/views.cpython-35.pyc 	modified:   lists/templates/home.html 	modified:   lists/tests.py 	modified:   lists/views.py

--- functional_tests.py
+++ functional_tests.py
@@ -7,11 +7,15 @@ class NewVisitorTest(unittest.TestCase):
     def setUp(self): 
         self.browser = webdriver.Firefox()
 
-
     def tearDown(self): 
         self.browser.quit()
 
-    def test_can_start_a_list_and_retrieve_is_later(self): 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
+    def test_can_start_a_list_and_retrieve_it_later(self): 
         # Edith has heard about a cool new online to-do app. She goes
         # to check out its homepage
         self.browser.get('http://localhost:8000')
@@ -36,18 +40,26 @@ class NewVisitorTest(unittest.TestCase):
         # "1: Buy peacock feathers" as an item in a to-do list
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
-
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows),
-            "New to-do item did not appear in table"
-            )
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         # There is still a text box inviting her to add anther item. She
         # enters "Use peacock feathers to make a fly" (Edith is very methodical)
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        # The page updates again, and now shows both item on her list
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+
+        # Edith wonders whether the site will remember her list. Then she sees
+        # thar he site has generated a unique URL for her -- there is some
+        # explanatory text to that effect.        
+        
         self.fail('Finish the test!')
 
+        # She visit thar URL - her to-do list is still there
         
 if __name__ == '__main__': 
     unittest.main(warnings='ignore') 
@@ -59,13 +71,11 @@ if __name__ == '__main__':
 
 
 
-# The page updates again, and now shows both item on her list
 
-# Edith wonders whether the site will remember her list. Then she sees
-# thar he site has generated a unique URL for her -- there is some
-# explanatory text to that effect.
 
-# She visit thar URL - her to-do list is still there
+
+
+
 
 # Satisfied, she goes back to sleep
 
